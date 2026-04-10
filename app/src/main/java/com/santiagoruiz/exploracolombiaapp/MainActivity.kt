5a0f21a0a5b2331c4d875edfd26df67d7cac6faa
Santiago Ruiz <sruiz557@unab.edu.co>
2026-04-10 11:16:41 -0500

    maquetacion login & register

--- app/src/main/java/com/santiagoruiz/exploracolombiaapp/MainActivity.kt
+++ app/src/main/java/com/santiagoruiz/exploracolombiaapp/MainActivity.kt
@@ -18,14 +18,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            ExploraColombiaAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            LoginScreen( onLoginSuccess = {}, onNavigateToRegister = {})
         }
     }
 }
